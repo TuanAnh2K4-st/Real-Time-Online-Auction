7cd78867e0cb1818e4993060e7e15d5f89101fa5
TuanAnh2K4-st <22130014@st.hcmuaf.edu.vn>
2026-03-08 21:36:53 +0700

    install tailwind axios router

---
+++ Fontend/src/routes/AppRoutes.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "../pages/Login";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+
+      <Routes>
+
+        <Route path="/login" element={<Login />} />
+
+      </Routes>
+
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
---
+++ Fontend/tailwind.config.js
@@ -0,0 +1,30 @@
+export default {
+  darkMode: "class",
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+        primary: "#137fec",
+        "background-light": "#f6f7f8",
+        "background-dark": "#101922",
+      },
+
+      fontFamily: {
+        display: ["Inter", "sans-serif"]
+      },
+
+      borderRadius: {
+        DEFAULT: "0.25rem",
+        lg: "0.5rem",
+        xl: "0.75rem",
+        full: "9999px"
+      }
+
+    },
+  },
+  plugins: [],
+}
